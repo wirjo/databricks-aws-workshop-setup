e17334a9ab244de9dbb1eaa607292f883069c586
wirjo <wirjo@amazon.com>
2024-04-30 10:29:21 +1000

    Fix metastore permission grants

--- db_metastore.tf
+++ db_metastore.tf
@@ -1,13 +1,20 @@
-resource "databricks_grants" "metastore_group_permissions" {
+resource "databricks_grant" "metastore_group_permissions" {
   provider  = databricks.workspace
   metastore = data.databricks_metastores.all.ids[var.databricks_metastore_name]
-
-  grant {
-    principal = databricks_group.aws_event.display_name
-    privileges = [
-      "CREATE_CATALOG",
-      "CREATE_EXTERNAL_LOCATION",
-      "CREATE_STORAGE_CREDENTIAL"
-    ]
-  }
+  principal = databricks_group.aws_event.display_name
+  privileges = [
+    "CREATE_CATALOG",
+    "CREATE_EXTERNAL_LOCATION",
+    "CREATE_STORAGE_CREDENTIAL"
+  ]
+}
+resource "databricks_grant" "metastore_service_principal_permissions" {
+  provider  = databricks.workspace
+  metastore = data.databricks_metastores.all.ids[var.databricks_metastore_name]
+  principal = var.service_principal_name
+  privileges = [
+    "CREATE_CATALOG",
+    "CREATE_EXTERNAL_LOCATION",
+    "CREATE_STORAGE_CREDENTIAL"
+  ]
 }
